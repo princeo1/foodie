44855e362f495829d14413b5d08b3ec8e05da18e
princeo1 <1996princesoni@gmail.com>
2025-03-04 13:30:17 +0530

    Making my cards dynamic

--- App.js
+++ App.js
@@ -2,6 +2,732 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
+const resList = [
+    {
+    "info": {
+    "id": "741531",
+    "name": "Burger King",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/1/9daf615c-b012-4e20-a431-83d143313108_741531.jpg",
+    "locality": "Siliguri",
+    "areaName": "City Centre Mall",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Burgers",
+    "American"
+    ],
+    "avgRating": 4.4,
+    "parentId": "166",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "1.7K+",
+    "sla": {
+    "deliveryTime": 20,
+    "lastMileTravel": 0.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "15-20 mins",
+    "lastMileTravelString": "0.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-05 02:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "Rxawards/_CATEGORY-Burger.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Rxawards/_CATEGORY-Burger.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/burger-king-city-centre-mall-rest741531",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "231961",
+    "name": "Subway",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/21/0bbc7b0c-38ad-4cb9-8147-382b40b4c517_231961.JPG",
+    "locality": "Matigara",
+    "areaName": "Patiram Jote",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "sandwich",
+    "Salads",
+    "wrap",
+    "Healthy Food"
+    ],
+    "avgRating": 4.2,
+    "parentId": "2",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "1.3K+",
+    "sla": {
+    "deliveryTime": 25,
+    "lastMileTravel": 0.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "0.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-05 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "20% OFF",
+    "subHeader": "UPTO ₹50"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/subway-matigara-patiram-jote-rest231961",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "297342",
+    "name": "Prasad Hotel",
+    "cloudinaryImageId": "8e87fd962bdb8b1a5573207466cc6320",
+    "locality": "Khalpara",
+    "areaName": "Siliguri Locality",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "North Indian",
+    "Chinese",
+    "Continental",
+    "Snacks",
+    "Thalis",
+    "Beverages"
+    ],
+    "avgRating": 4,
+    "parentId": "160651",
+    "avgRatingString": "4.0",
+    "totalRatingsString": "804",
+    "sla": {
+    "deliveryTime": 35,
+    "lastMileTravel": 4.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "35-40 mins",
+    "lastMileTravelString": "4.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-04 23:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹300 OFF",
+    "subHeader": "ABOVE ₹1499",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/prasad-hotel-khalpara-siliguri-locality-rest297342",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "1047543",
+    "name": "Shree Balaji South Indian Food",
+    "locality": "Janta Nagar",
+    "areaName": "Janta Nagar",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "South Indian"
+    ],
+    "veg": true,
+    "parentId": "298865",
+    "avgRatingString": "--",
+    "sla": {
+    "deliveryTime": 18,
+    "lastMileTravel": 0.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "15-20 mins",
+    "lastMileTravelString": "0.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-04 21:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "20% OFF",
+    "subHeader": "UPTO ₹50"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/shree-balaji-south-indian-food-janta-nagar-rest1047543",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "813678",
+    "name": "Anna Ka Dukaan",
+    "cloudinaryImageId": "18dd4490596019c8428d1a8f7dd20106",
+    "locality": "Rama karishna Samity",
+    "areaName": "Siliguri Locality",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "South Indian"
+    ],
+    "avgRating": 4.3,
+    "veg": true,
+    "parentId": "447458",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1.2K+",
+    "sla": {
+    "deliveryTime": 29,
+    "lastMileTravel": 5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "5.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-04 23:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "Green%20Dot%20Awards/Best%20In%20Dosa.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Green%20Dot%20Awards/Best%20In%20Dosa.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹79"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/anna-ka-dukaan-rama-karishna-samity-siliguri-locality-rest813678",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "212893",
+    "name": "Gokul Fresh",
+    "cloudinaryImageId": "qo1g0gco2aitzk3jaybw",
+    "locality": "Sevoke Road",
+    "areaName": "Bankim Nagar",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Desserts",
+    "South Indian",
+    "North Indian",
+    "Chinese",
+    "Snacks",
+    "Thalis"
+    ],
+    "avgRating": 4.4,
+    "veg": true,
+    "parentId": "88170",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "4.6K+",
+    "sla": {
+    "deliveryTime": 33,
+    "lastMileTravel": 6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "30-35 mins",
+    "lastMileTravelString": "6.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-04 21:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "Rxawards/_CATEGORY-South%20Indian.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Rxawards/_CATEGORY-South%20Indian.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/gokul-fresh-sevoke-road-bankim-nagar-rest212893",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "152542",
+    "name": "Swagat Sweets",
+    "cloudinaryImageId": "z9vuegnsslydctbircxq",
+    "locality": "Pradhan Nagar",
+    "areaName": "Hill Cart Road",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Desserts",
+    "Snacks"
+    ],
+    "avgRating": 4.4,
+    "veg": true,
+    "parentId": "198874",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "1.5K+",
+    "sla": {
+    "deliveryTime": 33,
+    "lastMileTravel": 4.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "30-35 mins",
+    "lastMileTravelString": "4.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-04 20:40:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "40% OFF",
+    "subHeader": "UPTO ₹80"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/swagat-sweets-pradhan-nagar-hill-cart-road-rest152542",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "226340",
+    "name": "Makhan Bhog",
+    "cloudinaryImageId": "wurawazafi6m7ahhvvep",
+    "locality": "Uttorayon",
+    "areaName": "Patiram Jote",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Indian",
+    "South Indian",
+    "Street Food",
+    "Chinese",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.4,
+    "veg": true,
+    "parentId": "8684",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "1.9K+",
+    "sla": {
+    "deliveryTime": 22,
+    "lastMileTravel": 0.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "0.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-04 21:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png",
+    "description": "bolt!"
+    },
+    {
+    "imageId": "Green%20Dot%20Awards/Best%20In%20Mishti.png",
+    "description": "Delivery!"
+    },
+    {
+    "imageId": "Rxawards/_CATEGORY-South%20Indian.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "bolt!",
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png"
+    }
+    },
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Green%20Dot%20Awards/Best%20In%20Mishti.png"
+    }
+    },
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Rxawards/_CATEGORY-South%20Indian.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "10% OFF",
+    "subHeader": "UPTO ₹40"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-60753905-27fe-4161-b296-df4ac6f8ad3e"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/siliguri/makhan-bhog-uttorayon-patiram-jote-rest226340",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+]
+
 const Header = () => {
     return <div className="header">
         <div className="logo">
@@ -31,9 +757,37 @@ const Header = () => {
     </div>
 }
 
+const Card = (props) => {
+    // console.log(props)
+    const {name,cuisines,avgRating,costForTwo,cloudinaryImageId} = props.resData.info
+    // const {resData} = props;
+    // console.log(resData.info.name)
+    return <div className="card">
+        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ cloudinaryImageId}
+        alt="food"
+        className="card-img"
+        />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(', ')}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo}</h4>
+    </div>
+}
+
+const CardContainer = () => {
+    return <div className="card-container">
+        {
+            resList.map( (restuarant)=> (
+                <Card key={restuarant.info.id} resData={restuarant}/>
+            ))
+        }
+    </div>
+}
+
 const App = () => {
     return <div className="app">
         <Header/>
+        <CardContainer/>
     </div>
 }
 
