9d17582b847484b6d3e696793356209ba1e563f5
princeo1 <1996princesoni@gmail.com>
2025-03-05 23:06:20 +0530

    Adding useEffect and getting the live data from swiggy's Api

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,5 +1,5 @@
 import RestaurantCard from "./RestaurantCard";
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import resList from "../utils/MockData";
 
 
@@ -100,6 +100,15 @@ const ListOfRestaurantJs = [
     //     }
     // ])
     const [ListOfRestaurant, setListOfRestaurant] = useState(resList);
+    useEffect(() => {
+        fetchData();
+    },[])
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const json = await data.json();
+        setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        // console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
     return <div className="body">
         <div className="filter">
             <button
