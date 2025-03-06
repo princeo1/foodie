5affa12aa08749f32731b16ad273dbc5153d3b50
princeo1 <1996princesoni@gmail.com>
2025-03-06 18:07:28 +0530

    Adding search fnctionality

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,17 +5,29 @@ import Shimmer from "./Shimmer";
 
  const Body = () => {
     const [ListOfRestaurant, setListOfRestaurant] = useState([]);
+    const [filteredRestaurant, setfilteredRestaurant] = useState([])
+    const [searchText, setsearchText] = useState('');
     useEffect(() => {
         fetchData();
     },[])
     const fetchData = async () => {
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
         const json = await data.json();
+        console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setfilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
-    
+
     return ListOfRestaurant.length === 0 ? <Shimmer/> :<div className="body">
         <div className="filter">
+            <input type="text" onChange={(e)=>setsearchText(e.target.value)}/>
+            <button
+                className="search-btn"
+                onClick={()=> {
+                    const filteredResList = ListOfRestaurant.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+                    setfilteredRestaurant(filteredResList)
+                }}
+            >Search</button>
             <button
             className="filter-btn"
             onClick={() => {
@@ -30,7 +42,7 @@ import Shimmer from "./Shimmer";
         </div>
         <div className="card-container">
             {
-                ListOfRestaurant.map( (restuarant)=> (
+                filteredRestaurant.map( (restuarant)=> (
                     <RestaurantCard key={restuarant.info.id} resData={restuarant}/>
                 ))
             }
