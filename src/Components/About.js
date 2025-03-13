ee695d384e2606f8f13ee4a8b2daabc9204a0faa
princeo1 <1996princesoni@gmail.com>
2025-03-13 09:07:05 +0530

    creating class based components

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
-        <h1>I am about Page</h1>
+        <div>
+            <h1>I am about Page</h1>
+            <User name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
+            <UserClass name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
+        </div>
     )
 }
 export default About;
---
+++ src/Components/UserClass.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class UserClass extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class="user-card">
+                <h2>Name : {this.props.name}</h2>
+                <h3>Location: {this.props.location}</h3>
+                <h4>Contact : {this.props.contact}</h4>
+            </div>
+        )
+    }
+}
+
+export default UserClass;
