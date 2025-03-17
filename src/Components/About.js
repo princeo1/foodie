254728a4418098d1f8e919a5594966543fc458e4
princeo1 <1996princesoni@gmail.com>
2025-03-17 13:55:50 +0530

    making about.js a class component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,34 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from 'react'
 
-const About = () => {
-    return (
-        <div>
-            <h1>I am about Page</h1>
-            <User name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
-            <UserClass name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
-        </div>
-    )
+class About extends React.Component {
+    constructor () {
+        super()
+        console.log('About constructor')
+    }
+    componentDidMount() {
+        console.log('About Component did mount')
+    }
+    render () {
+        console.log('About render')
+        return (
+            <div>
+                <h1>I am about Page</h1>
+                <User name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
+                <UserClass name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
+            </div>
+        )
+    }
 }
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>I am about Page</h1>
+//             <User name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
+//             <UserClass name={"Prince Soni"} location={"Jaigaon"} contact="prince_oo1"/>
+//         </div>
+//     )
+// }
 export default About;
--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -3,16 +3,26 @@ import React from 'react'
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        console.log('User Constructor')
         this.state = {
             count: 3,
             count2: 4,
         }
     }
+    componentDidMount () {
+        console.log('User componentDidMount')
+    }
     render() {
+        console.log('User Render')
         return (
             <div class="user-card">
                 <h1>Count = {this.state.count} </h1>
                 <h1>Count2 = {this.state.count2}</h1>
+                <button onClick={()=> {
+                    this.setState({
+                        count: this.state.count + 1
+                    })
+                }}>Increase Count</button>
                 <h2>Name : {this.props.name}</h2>
                 <h3>Location: {this.props.location}</h3>
                 <h4>Contact : {this.props.contact}</h4>
