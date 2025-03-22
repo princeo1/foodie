b882e49a7a1b7c42d6cd6139b8054f89816d862c
princeo1 <1996princesoni@gmail.com>
2025-03-22 19:54:36 +0530

    Adding initial header styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import { LOGO_URL } from "../utils/constants"
 import { useState,useEffect } from 'react'
 import { Link } from 'react-router'
-import useOnlineStatus from '../utils/useOnlineStatus' 
+import useOnlineStatus from '../utils/useOnlineStatus'
 
 const Header = () => {
     const [loginBtn, setloginBtn] = useState('login');
@@ -10,47 +10,40 @@ const Header = () => {
     useEffect(()=> {
         console.log('useEffect called')
     },[loginBtn])
-    return <div className="header">
+    return <div className="flex justify-between bg-green-100 items-center">
         <div className="logo">
             <img
-                className="nav-logo"
+                className="nav-logo h-[80px] w-[80px] ml-15"
                 src={LOGO_URL}
                 alt="logo"
             />
         </div>
-        <div className="navbar">
+        <div className="flex text-lg">
                 <div className="nav-item">
-                    <div>Online Status : {onlineStatus ? 'Online' : 'Offline'}</div>
+                    <div className="px-10">Online Status : {onlineStatus ? 'Online' : 'Offline'}</div>
                 </div>
-                <div className="nav-item">
-                    <img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
-                    className="corporate-icon"/>
+                <div className="flex px-10">
                     <Link to="./groceries">Groceries</Link>
                 </div>
-                <div className="nav-item"><img src="https://cdn-icons-png.flaticon.com/512/4440/4440484.png"
-                 className="corporate-icon"/>
+                <div className="flex px-10">
                  <Link to="./"> Home </Link>
                 </div>
-                <div className="nav-item"><img src="https://cdn-icons-png.flaticon.com/512/2956/2956869.png"
-                 className="corporate-icon"/>
+                <div className="flex px-10">
                  <Link to="./offers"> Offers </Link>
                 </div>
-                <div className="nav-item"><img src="https://icons.veryicon.com/png/o/miscellaneous/flat-icon/help-252.png"
-                 className="corporate-icon"/>
+                <div className="flex px-10">
                  <Link to="./about"> About </Link>
                 </div>
-                <div className="nav-item"><img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
-                 className="corporate-icon"/>
+                <div className="flex px-10">
                  <Link to="cart"> Cart </Link>
                 </div>
                 <div 
-                className="nav-item"
+                className="px-10"
                 onClick={() => {
                     console.log('login btn clicked')
                     loginBtn === 'login' ? setloginBtn('logout') : setloginBtn('login')
                  }}
                  >
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRylqFwcq_UkroUbJ0rD0_BYS1cCb2tqTimwA&s" className="corporate-icon" />
                 {loginBtn}
                 </div>
         </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,20 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 export const LOGO_URL = "https://t3.ftcdn.net/jpg/08/29/90/88/360_F_829908823_kYsRKdQcIaYEAhHRAZTIXuSKvuVPif8w.jpg"
 export const RES_URL_PRE = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
 export const RES_URL_POST = "&catalog_qa=undefined&submitAction=ENTER"
+
+
+/*
+
+<img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
+                    className="w-8"/>
+<img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
+                 className="w-10"/>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRylqFwcq_UkroUbJ0rD0_BYS1cCb2tqTimwA&s" className="w-10" />
+<img src="https://icons.veryicon.com/png/o/miscellaneous/flat-icon/help-252.png"
+                 className="w-10"/>
+<img src="https://cdn-icons-png.flaticon.com/512/2956/2956869.png"
+                 className="w-10"/>
+<img src="https://cdn-icons-png.flaticon.com/512/4440/4440484.png"
+                 className="w-10"/>
+
+*/
