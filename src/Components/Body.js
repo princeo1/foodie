8dc9a7fd790893626c85734647f99b9be823f4c1
princeo1 <1996princesoni@gmail.com>
2025-04-15 23:38:40 +0530

    Adding a prefix to solve cors issue

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -15,7 +15,7 @@ import useOnlineStatus from "../utils/useOnlineStatus";
         fetchData();
     },[])
     const fetchData = async () => {
-        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const data = await fetch('https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
         const json = await data.json();
         console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+export const CDN_URL = "https://thingproxy.freeboard.io/fetch/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 export const LOGO_URL = "https://t3.ftcdn.net/jpg/08/29/90/88/360_F_829908823_kYsRKdQcIaYEAhHRAZTIXuSKvuVPif8w.jpg"
-export const RES_URL_PRE = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+export const RES_URL_PRE = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
 export const RES_URL_POST = "&catalog_qa=undefined&submitAction=ENTER"
 export const DISH_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
 
